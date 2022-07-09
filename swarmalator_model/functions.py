94ef0858a30186e74c63693637235d3cb0bc5b59
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-07-09 14:44:17 +0200

    switched to numpy arrays for better performance

--- swarmalator_model/functions.py
+++ swarmalator_model/functions.py
@@ -1,4 +1,6 @@
-import tkinter, threading, math, colorsys, time
+from ast import arg
+from concurrent.futures import thread
+import tkinter, threading, time
 from swarmalator_model.swarmalator import Swarmalator
 
 def initialise_canvas(window, screen_size):
@@ -36,42 +38,9 @@ def create_swarmalators(canvas, no_of_swarmalators, screen_size):
     for n in range(no_of_swarmalators):
         swarmalator = Swarmalator(n, no_of_swarmalators)
         list_of_swarmalators.append(swarmalator)
+        swarmalator.draw_swarmalator(canvas, screen_size)
 
     return list_of_swarmalators
-
-def initialize_status(list_of_swarmalators):
-    '''Fills the global position an phase dict.
-
-    Parameters
-    ----------
-        list_of_swarmalators (list[swarmalator_model.Swarmalator]): List of swarmalator objects
-    
-    Returns
-    ----------
-        positions (dict): Positions of swarmaltors in the system.
-        phases (dict): Phases of swarmaltors in the system.
-    '''
-    positions = {}
-    phases = {}
-
-    for s in list_of_swarmalators:
-        positions[s.id] = s.position
-        phases[s.id] = s.phase
-
-    return positions, phases
-
-def update_status(list_of_swarmalators, positions, phases):
-    '''Updates the global position an phase dicts.
-
-    Parameters
-    ----------
-        list_of_swarmalators (list[swarmalator_model.Swarmalator]): List of swarmalator objects
-        positions (dict): Positions of swarmaltors in the system.
-        phases (dict): Phases of swarmaltors in the system.
-    '''
-    for s in list_of_swarmalators:
-        positions[s.id] = s.position
-        phases[s.id] = s.phase
 
 def run_simulation(screen_size, no_of_swarmalators, delta_t, J, K, coupling_probability):
     '''Initiates the main loop of the simulation.
@@ -86,24 +55,20 @@ def run_simulation(screen_size, no_of_swarmalators, delta_t, J, K, coupling_prob
         coupling_probability (float): Probability for a swarmalator to update its information about neighbours (default `0.01`)
     ''' 
     sim = tkinter.Tk()
-    sim.title('Swarmalators with Stochastic Coupling 2022 D. Kofler')
     canvas = initialise_canvas(sim, screen_size)
     list_of_swarmalators = create_swarmalators(canvas, no_of_swarmalators, screen_size)
-    positions, phases = initialize_status(list_of_swarmalators)
 
-    draw_swarmalators(canvas, screen_size, positions, phases)
+    step(canvas, list_of_swarmalators, screen_size, delta_t, J, K, coupling_probability)
 
-    step(canvas, list_of_swarmalators, positions, phases, screen_size, delta_t, J, K, coupling_probability)
     sim.mainloop()
 
-def step(canvas, list_of_swarmalators, positions, phases, screen_size, delta_t, J, K, coupling_probability):
+def step(canvas, list_of_swarmalators, screen_size, delta_t, J, K, coupling_probability):
     '''Updates the canas and all swarmalators.
 
     Parameters
     ----------
         canvas (tikinter.Canvas): Canvas object the swarmalators should be added to.
-        positions (dict): Positions of swarmaltors in the system.
-        phases (dict): Phases of swarmaltors in the system.
+        list_of_swarmalators (list[swarmalator_model.Swarmalator]): List of swarmalator objects
         screen_size (int): size of the canas
         delta_t (float): value of one euler step, rate at which the canas updates
         J (float): Parameter that influences the attraction and repulsion between swarmalators
@@ -112,53 +77,11 @@ def step(canvas, list_of_swarmalators, positions, phases, screen_size, delta_t, 
     '''    
     canvas.delete("all")
 
-    start = time.time()  
     for swarmalator in list_of_swarmalators:
-<<<<<<< HEAD
         swarmalator.step(list_of_swarmalators, delta_t, J, K, coupling_probability)
-=======
-        # swarmalator.step(positions, phases, delta_t, J, K, coupling_probability)
-        t = threading.Thread(target=swarmalator.step, args=(positions, phases, delta_t, J, K, coupling_probability), daemon=True)
-        threads.append(t)
-        t.start()
 
-    for t in threads:
-        t.join()
-    
-    update_status(list_of_swarmalators, positions, phases)
-    draw_swarmalators(canvas, screen_size, positions, phases)
-    end = time.time()
-    calc_time = end - start
-    print(f'calc time {calc_time} s ...')
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
+    for swarmalator in list_of_swarmalators:
+        swarmalator.draw_swarmalator(canvas, screen_size)
 
-    delay = int(delta_t * 1000 - calc_time)
-    canvas.after(delay, step, canvas, list_of_swarmalators, positions, phases, screen_size, delta_t, J, K, coupling_probability)
-
-def draw_swarmalators(canvas, screen_size, positions, phases):
-    '''Draws all swarmalators on the canvas.
-
-    Parameters
-    ----------
-        canvas (tikinter.Canvas): Canvas object the swarmalators should be added to.
-        screen_size (int): size of the canvas
-        positions (dict): Positions of swarmaltors in the system.
-        phases (dict): Phases of swarmaltors in the system.
-
-    '''
-    size = 10
-
-    for p in positions:
-        # color based on swarmalator phase
-        c_val = phases[p] / (2.0 * math.pi) % (2.0 * math.pi)
-        c_rgb = colorsys.hsv_to_rgb(c_val, 1, 1)
-        c_int = tuple(int(t * 255) for t in c_rgb)
-        c_hex = '#%02x%02x%02x' % c_int
-
-        pos = positions[p]
-        canv_pos_x = pos[0] * screen_size / 4 + screen_size / 2
-        canv_pos_y = pos[1] * screen_size / 4 + screen_size / 2
-        x1 = canv_pos_x + size
-        x2 = canv_pos_y + size
-
-        canvas.create_oval(canv_pos_x, canv_pos_y, x1, x2, fill=c_hex, tags=p)
+    delay = int(delta_t * 1000)
+    canvas.after(delay, step, canvas, list_of_swarmalators, screen_size, delta_t, J, K, coupling_probability)
--- swarmalator_model/swarmalator.py
+++ swarmalator_model/swarmalator.py
@@ -1,6 +1,8 @@
 import random as rnd
 import math
+import colorsys
 import numpy as np
+
 
 class Swarmalator:
     def __init__(self, id, num_of_swarmalators):
@@ -15,7 +17,6 @@ class Swarmalator:
 
         self.velocity = np.zeros(2)
         self.d_phase = 0
-<<<<<<< HEAD
 
         self.phases = np.zeros(num_of_swarmalators) #phase-vector
         self.positions = np.zeros((num_of_swarmalators, 2)) #position-array
@@ -49,12 +50,6 @@ class Swarmalator:
         canvas.create_oval(canv_pos_x, canv_pos_y, x1, x2, fill=c_hex, tags='s' + str(self.id))
 
     def step(self, list_of_swarmalators, delta_t, J, K, coupling_probability):
-=======
-        self.neighbour_phases = {}
-        self.neighbour_positions = {}
-
-    def step(self, positions, phases, delta_t, J, K, coupling_probability):
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
         '''Makes the swarmalator sync, update and move.
 
         Parameters
@@ -67,13 +62,8 @@ class Swarmalator:
             K (float): Parameter that influences the phase synchronization between swarmalators
             coupling_probability (float): Probability for a swarmalator to update its information about neighbours
         '''
-<<<<<<< HEAD
         self.synchronize(list_of_swarmalators, coupling_probability)
         self.update(len(list_of_swarmalators), J, K)
-=======
-        self.synchronize(positions, phases, coupling_probability)
-        self.think(positions, phases, J, K)
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
         self.move(delta_t)
 
     def move(self, delta_t):
@@ -90,13 +80,8 @@ class Swarmalator:
         # update phase
         self.phases[self.id] = (self.phases[self.id] + self.d_phase * delta_t) % (2.0 * math.pi)
     
-<<<<<<< HEAD
     def update(self, num_of_swarmalators, J, K):
         '''Updates the position and phase of a swarmalator based on it's neighbours.
-=======
-    def think(self, positions, phases, J, K):
-        '''Updates the position and pahse of a swarmalator based on it's neighbours.
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
 
         Parameters
         ----------
@@ -116,27 +101,10 @@ class Swarmalator:
                 v_temp += (d_x / d_x_norm * (1.0 + J * math.cos(d_theta)) - d_x / (d_x_norm * d_x_norm))
                 p_temp += math.sin(d_theta) / d_x_norm
 
-<<<<<<< HEAD
         self.velocity = 1 / (len(self.positions)) * v_temp
         self.d_phase = K / (len(self.phases)) * p_temp
-=======
-            for p in positions:
-                if self.id != p and p in phases:
-                    x_j = positions[p]
-                    theta_j = phases[p]
-                    
-                    d_x = x_j - x_i
-                    d_theta = theta_j - theta_i
-                    d_x_norm = np.linalg.norm(d_x)
-                    
-                    v_temp += (d_x / d_x_norm * (1.0 + J * math.cos(d_theta)) - d_x / (d_x_norm * d_x_norm))
-                    p_temp += math.sin(d_theta) / d_x_norm
 
-            self.velocity = 1 / (len(self.neighbour_positions)) * v_temp
-            self.d_phase = K / (len(self.neighbour_phases)) * p_temp
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
-
-    def synchronize(self, positions, phases, coupling_probability):
+    def synchronize(self, list_of_swarmalators, coupling_probability):
         '''Updates a swarmalator's memory of positions and phases of it's neighbours.
 
         Parameters
@@ -144,17 +112,9 @@ class Swarmalator:
             list_of_swarmalators (list[swarmalator_model.Swarmalator]): List of swarmalator objects
             coupling_probability (float): Probability for a swarmalator to update its information about neighbours
         '''
-<<<<<<< HEAD
         for i in range(len(self.positions)):
             if i != self.id:
                 r = rnd.random()
                 if r <= coupling_probability:
                     self.positions[i] = list_of_swarmalators[i].positions[i]
                     self.phases[i] = list_of_swarmalators[i].phases[i]
-=======
-        r = rnd.random()
-        for p in positions:
-            if p != self.id and r <= coupling_probability: self.neighbour_positions[p] = positions[p]
-        for p in phases:
-            if p != self.id and r <= coupling_probability: self.neighbour_phases[p] = phases[p]
->>>>>>> 347b629196238f5cbec67f52fe036be96e460b18
