636ab4951d0e650cbccbf1883a8c28784fa5e832
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-08-02 19:17:07 +0200

    draw coordinate system at init

--- swarmalator_model/simulation.py
+++ swarmalator_model/simulation.py
@@ -36,13 +36,13 @@ class Simulation:
         self.canvas.grid(row=0, column=0, rowspan=15)
 
         # Entry Number of Swarmalators
-        tk.Label(self.sim, text='Number of Swarmalators').grid(row=0, column=1, sticky='w')
+        tk.Label(self.sim, text='Number of swarmalators').grid(row=0, column=1, sticky='w')
         self.entry_num_swarmalators = tk.Entry(self.sim)
         self.entry_num_swarmalators.insert(0, "100")
         self.entry_num_swarmalators.grid(row=0, column=2)
 
         # Entry Memory Init Method
-        tk.Label(self.sim, text='Initialization Method').grid(row=1, column=1, sticky='w')
+        tk.Label(self.sim, text='Memory initialization').grid(row=1, column=1, sticky='w')
         self.var_memory_init = tk.StringVar(self.sim, 'random')
         tk.Radiobutton(self.sim, text='random', variable=self.var_memory_init, value='random').grid(row=1, column=2)
         tk.Radiobutton(self.sim, text='zeroes', variable=self.var_memory_init, value='zeroes').grid(row=1, column=3)
@@ -72,27 +72,27 @@ class Simulation:
         self.entry_K.insert(0, "1.0")
         self.entry_K.grid(row=5, column=2)
 
-        # Entry Simulation Type
-        tk.Label(self.sim, text='Simulation Type').grid(row=6, column=1, sticky='w')
-        self.var_simulation_type = tk.StringVar(self.sim, 'positions')
-        tk.Radiobutton(self.sim, text='positions', variable=self.var_simulation_type, value='positions').grid(row=6, column=2)
-        tk.Radiobutton(self.sim, text='phases', variable=self.var_simulation_type, value='phases').grid(row=6, column=3)
+        # Entry Plot Type
+        tk.Label(self.sim, text='Plot type').grid(row=6, column=1, sticky='w')
+        self.var_plot_type = tk.StringVar(self.sim, 'positions')
+        tk.Radiobutton(self.sim, text='positions', variable=self.var_plot_type, value='positions', command=self.draw_coordinate_system).grid(row=6, column=2)
+        tk.Radiobutton(self.sim, text='phases', variable=self.var_plot_type, value='phases', command=self.draw_coordinate_system).grid(row=6, column=3)
 
         # Button Start
-        self.btn_start = tk.Button(self.sim, text='Start/Reset Simulation', command=self.start_simulation)
+        self.btn_start = tk.Button(self.sim, text='Start/Reset simulation', command=self.start_simulation)
         self.btn_start.grid(row=8, column=1)
 
         # Button Stop
-        self.btn_stop = tk.Button(self.sim, text='Stop Simulation', command=self.stop_simulation)
+        self.btn_stop = tk.Button(self.sim, text='Stop simulation', command=self.stop_simulation)
         self.btn_stop.grid(row=8, column=2)
         self.btn_stop.config(state='disabled')
 
         # Button Pause
-        self.btn_pause = tk.Button(self.sim, text='Pause Simulation', command=self.pause_simulation)
+        self.btn_pause = tk.Button(self.sim, text='Pause simulation', command=self.pause_simulation)
         self.btn_pause.grid(row=9, column=1)
 
         # Button Show Data
-        tk.Button(self.sim, text='Save Data', command=self.save_data).grid(row=10, column=1)
+        tk.Button(self.sim, text='Save data', command=self.save_data).grid(row=10, column=1)
 
         # Label Iteration
         self.lbl_iteration = tk.Label(self.sim, text='iteration = 1')
@@ -120,7 +120,7 @@ class Simulation:
             self.coupling_probability = round(float(self.entry_coupling_probability.get()), 2)
             self.J = round(float(self.entry_J.get()), 2)
             self.K = round(float(self.entry_K.get()), 2)
-            self.simulation_type = str(self.var_simulation_type.get())
+            self.simulation_type = str(self.var_plot_type.get())
             return True
         except:
             print('Error reading inputs.')
@@ -130,21 +130,22 @@ class Simulation:
         '''
         Starts the main loop.
         '''
+        self.draw_coordinate_system()
         self.sim.mainloop()
     
     def start_simulation(self):
         '''
         Starts a simulation run.
         '''
+        if not self.read_inputs(): return
         self.paused = False
         self.stopped = False
         self.iteration = 1
         self.simulaton_time = 0
         self.btn_pause['text'] = 'Pause Simulation'
-        self.canvas.delete('all')
         self.btn_start.config(state='disabled')
         self.btn_stop.config(state='active')
-        if not self.read_inputs(): return
+
         self.draw_coordinate_system()
         self.canvas.update()
         self.init_swarmalators()
@@ -184,6 +185,7 @@ class Simulation:
         Shows positions and velocities of each swarmalator.
         '''
         print(pd.DataFrame(self.memory))
+        print(pd.DataFrame(self.velocities))
 
     def init_swarmalators(self):
         '''
@@ -199,7 +201,7 @@ class Simulation:
         Makes each swarmalator perform one step of syncing and moving.
         '''
         wait_time = int(self.time_step * 1000)
-        self.simulation_type = str(self.var_simulation_type.get()) # read simulation type input to make live-switching possible
+        self.simulation_type = str(self.var_plot_type.get()) # read simulation type input to make live-switching possible
 
         if not self.stopped:
             if not self.paused:
@@ -241,6 +243,8 @@ class Simulation:
         Draws a coordinate system on the canvas.
         '''
         areas = 8
+        self.canvas.delete('all')
+        self.simulation_type = str(self.var_plot_type.get())
         
         for i in range(1, areas):
             x_x0 = y_y0 = 0.0
@@ -265,20 +269,21 @@ class Simulation:
         '''
         Draws swarmalators on the canvas based on their position.
         '''
-        size = self.plot_size / 100
+        size = self.plot_size / 120
         for i in range(self.num_swarmalators):
             color = fct.phase_to_hex(self.memory[i][2])
 
             x1 = self.plot_size * ((self.memory[i][0] + 2.0 ) / 4.0)
-            y1 = self.plot_size * ((self.memory[i][1] + 2.0 ) / 4.0)
+            y1 = (self.plot_size * ((-self.memory[i][1] + 2.0 ) / 4.0))
 
             diff_vec = self.velocities[i] / np.linalg.norm(self.velocities[i]) * size
             x2 = x1 + diff_vec[0]
-            y2 = y1 + diff_vec[1]
+            y2 = y1 - diff_vec[1]
 
             self.canvas.create_line(
-                x2, y2, x1, y1, fill=color, tags='s',
-                arrow=tk.FIRST, arrowshape=(8 * size / 5, 10 * size / 5, 3 * size / 5))
+                x1, y1, x2, y2, fill=color, tags='s',
+                arrow=tk.LAST, arrowshape=(8 * size / 5, 10 * size / 5, 3 * size / 5))
+            # self.canvas.create_text(x2, y2, text=str(i), tags='s')
 
     def draw_phases(self):
         '''
@@ -291,8 +296,8 @@ class Simulation:
             elif self.memory[i][0] < 0 and self.memory[i][1] < 0: a -= math.pi
 
             x1 = self.plot_size * ((a / math.pi + 1.0 ) / 2.0)
-            y1 = self.plot_size * ((self.memory[i][2] / math.pi + 1.0 ) / 2.0)
+            y1 = self.plot_size * ((-self.memory[i][2] / math.pi + 1.0 ) / 2.0)
             x2 = x1 + size
             y2 = y1 + size
 
-            self.canvas.create_oval(x2, y2, x1, y1, fill='black', tags='s')
+            self.canvas.create_oval(x1, y1, x2, y2, fill='black', tags='s')
