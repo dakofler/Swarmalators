d51a6dce8e00e6e5a0beb4f6c01c2196e3253acd
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-09-18 15:43:48 +0200

    gui refinements

--- swarmalator_model/simulation.py
+++ swarmalator_model/simulation.py
@@ -67,7 +67,7 @@ class Simulation:
         self.paused = False
         self.stopped = True
 
-        self.__init_canvas(num_swarmalators, memory_init, time_step, coupling_probability, J, K, plot_type)
+        self.__init_canvas(num_swarmalators, memory_init, time_step, coupling_probability, J, K, alpha, plot_type)
 
     #region Core functions
     def run_simulation(self):
@@ -135,7 +135,7 @@ class Simulation:
     #endregion
 
     #region Initialization
-    def __init_canvas(self, num_swarmalators: int, memory_init: str, time_step: float, coupling_probability: float, J: float, K: float, plot_type: str):
+    def __init_canvas(self, num_swarmalators: int, memory_init: str, time_step: float, coupling_probability: float, J: float, K: float, alpha: float, plot_type: str):
         '''
         Initializes the environment canvas object and all control elements.
 
@@ -156,97 +156,134 @@ class Simulation:
             Phase attraction strength. For J > 0 swarmalators with similar phases attract each other. For J < 0 opposite phased swarmalators are attracted.
         K : float
             Phase coupling strength. For K > 0 swarmalators try to minimize their phase difference. For K < 0 the difference is maximized.
+        alpha : float
+            Momentum factor. Must be between 0 and 1.
         plot_type : {'positions', 'phases'}, optional
             Type of data to be displayed.
         '''
+        # Window setup
         self.sim = ctk.CTk()
+        window_width = self.plot_size + 750
+        window_height = self.plot_size + 100
+        screen_width = self.sim.winfo_screenwidth()
+        screen_height = self.sim.winfo_screenheight()
+        center_x = int(screen_width/2 - window_width / 2)
+        center_y = int(screen_height/2 - window_height / 2)
+        self.sim.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+        self.sim.resizable(False, False)
+        
+        self.sim.configure(bg='white')
+        self.sim.title('Swarmalators')
+
+        self.sim.columnconfigure(0, weight=5)
+        self.sim.columnconfigure(4, weight=5)
+        self.sim.columnconfigure(5, weight=5)
+        self.sim.columnconfigure(6, weight=5)
+        self.sim.rowconfigure(0, weight=5)
+        self.sim.rowconfigure(13, weight=5)
 
         ctk.set_appearance_mode("light")  # Modes: system (default), light, dark
         ctk.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
         
         # Canvas
-        self.canvas = ctk.CTkCanvas(master=self.sim, width=self.plot_size, height=self.plot_size)
-        self.canvas.grid(row=0, column=0, rowspan=15)
+        self.canvas = ctk.CTkCanvas(master=self.sim, width=self.plot_size, height=self.plot_size, bg='white')
+        self.canvas.grid(row=1, column=1, rowspan=12, columnspan=3)
 
         # Entry Number of Swarmalators
-        ctk.CTkLabel(self.sim, text='Number of swarmalators').grid(row=0, column=1, sticky='w')
-        
-        # self.entry_num_swarmalators = ctk.CTkEntry(self.sim)
-        # self.entry_num_swarmalators.insert(0, str(num_swarmalators))
-        # self.entry_num_swarmalators.grid(row=0, column=2)
+        ctk.CTkLabel(self.sim, text='Number of swarmalators').grid(row=1, column=7, sticky=tk.W)
 
-        self.lbl_num_swarmalators = ctk.CTkLabel(self.sim, text=str(num_swarmalators))
-        self.lbl_num_swarmalators.grid(row=0, column=2, sticky='w')
         self.slider_num_swarmalators = ctk.CTkSlider(self.sim, from_=50, to=500, number_of_steps=45, command=lambda v: self.lbl_num_swarmalators.configure(text=str(int(v))))
         self.slider_num_swarmalators.set(num_swarmalators)
-        self.slider_num_swarmalators.grid(row=0, column=3)
+        self.slider_num_swarmalators.grid(row=1, column=8)
+
+        self.lbl_num_swarmalators = ctk.CTkLabel(self.sim, text=str(num_swarmalators))
+        self.lbl_num_swarmalators.grid(row=1, column=9)
 
         # Entry Memory Init Method
-        ctk.CTkLabel(self.sim, text='Memory initialization').grid(row=1, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='Memory initialization').grid(row=2, column=7, sticky=tk.W)
+
         self.var_memory_init = tk.StringVar(self.sim, memory_init)
-        ctk.CTkRadioButton(self.sim, text='random', variable=self.var_memory_init, value='random').grid(row=1, column=2)
-        ctk.CTkRadioButton(self.sim, text='zeroes', variable=self.var_memory_init, value='zeroes').grid(row=1, column=3)
-        ctk.CTkRadioButton(self.sim, text='gradual', variable=self.var_memory_init, value='gradual').grid(row=1, column=4)
+        ctk.CTkRadioButton(self.sim, text='random', variable=self.var_memory_init, value='random').grid(row=2, column=8)
+        ctk.CTkRadioButton(self.sim, text='zeroes', variable=self.var_memory_init, value='zeroes').grid(row=3, column=8)
+        ctk.CTkRadioButton(self.sim, text='gradual', variable=self.var_memory_init, value='gradual').grid(row=4, column=8)
 
         # Entry Time Step
-        ctk.CTkLabel(self.sim, text='Time step in s').grid(row=2, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='Time step in s').grid(row=5, column=7, sticky=tk.W)
+
         self.entry_time_step = ctk.CTkEntry(self.sim)
         self.entry_time_step.insert(0, str(time_step))
-        self.entry_time_step.grid(row=2, column=2)
+        self.entry_time_step.grid(row=5, column=8)
 
         # Entry Coupling Probabiltity
-        ctk.CTkLabel(self.sim, text='Coupling probability').grid(row=3, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='Coupling probability').grid(row=6, column=7, sticky=tk.W)
+
         self.entry_coupling_probability = ctk.CTkEntry(self.sim)
         self.entry_coupling_probability.insert(0, str(coupling_probability))
-        self.entry_coupling_probability.grid(row=3, column=2)
+        self.entry_coupling_probability.grid(row=6, column=8)
 
         # Entry J
-        ctk.CTkLabel(self.sim, text='J').grid(row=4, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='J').grid(row=7, column=7, sticky=tk.W)
+
         self.entry_J = ctk.CTkEntry(self.sim)
         self.entry_J.insert(0, str(J))
-        self.entry_J.grid(row=4, column=2)
+        self.entry_J.grid(row=7, column=8)
 
         # Entry K
-        ctk.CTkLabel(self.sim, text='K').grid(row=5, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='K').grid(row=8, column=7, sticky=tk.W)
+
         self.entry_K = ctk.CTkEntry(self.sim)
         self.entry_K.insert(0, str(K))
-        self.entry_K.grid(row=5, column=2)
+        self.entry_K.grid(row=8, column=8)
+
+        # Entry alpha
+        ctk.CTkLabel(self.sim, text='alpha').grid(row=9, column=7, sticky=tk.W)
+
+        self.entry_alpha = ctk.CTkEntry(self.sim)
+        self.entry_alpha.insert(0, str(alpha))
+        self.entry_alpha.grid(row=9, column=8)
 
         # Entry Plot Type
-        ctk.CTkLabel(self.sim, text='Plot type').grid(row=6, column=1, sticky='w')
+        ctk.CTkLabel(self.sim, text='Plot type').grid(row=11, column=7, sticky='w')
+
         self.var_plot_type = tk.StringVar(self.sim, plot_type)
-        ctk.CTkRadioButton(self.sim, text='positions', variable=self.var_plot_type, value='positions', command=self.__draw_coordinate_system).grid(row=6, column=2)
-        ctk.CTkRadioButton(self.sim, text='phases', variable=self.var_plot_type, value='phases', command=self.__draw_coordinate_system).grid(row=6, column=3)
+        ctk.CTkRadioButton(self.sim, text='positions', variable=self.var_plot_type, value='positions', command=self.__draw_coordinate_system).grid(row=11, column=8)
+        ctk.CTkRadioButton(self.sim, text='phases', variable=self.var_plot_type, value='phases', command=self.__draw_coordinate_system).grid(row=12, column=8)
 
         # Button Start
-        self.btn_start = ctk.CTkButton(self.sim, text='Start/Reset simulation', command=self.__start_simulation)
-        self.btn_start.grid(row=8, column=1)
+        self.btn_start = ctk.CTkButton(self.sim, text='Start', command=self.__start_simulation)
+        self.btn_start.grid(row=1, column=5)
 
         # Button Stop
-        self.btn_stop = ctk.CTkButton(self.sim, text='Stop simulation', command=self.__stop_simulation)
-        self.btn_stop.grid(row=8, column=2)
+        self.btn_stop = ctk.CTkButton(self.sim, text='Stop', command=self.__stop_simulation)
+        self.btn_stop.grid(row=3, column=5)
         self.btn_stop.configure(state=tk.DISABLED)
 
         # Button Pause
-        self.btn_pause = ctk.CTkButton(self.sim, text='Pause simulation', command=self.__pause_simulation)
-        self.btn_pause.grid(row=9, column=1)
+        self.btn_pause = ctk.CTkButton(self.sim, text='Pause', command=self.__pause_simulation)
+        self.btn_pause.grid(row=5, column=5)
 
         # Button Save Data
-        ctk.CTkButton(self.sim, text='Save data', command=self.__save_data).grid(row=10, column=1)
+        ctk.CTkButton(self.sim, text='Save', command=self.__save_data).grid(row=7, column=5)
 
         # Label Iteration
-        self.lbl_iteration = ctk.CTkLabel(self.sim, text='iteration = 1')
-        self.lbl_iteration.grid(row=11, column=1, sticky='w')
+        self.lbl_iteration = ctk.CTkLabel(self.sim, text='Iteration 1')
+        self.lbl_iteration.grid(row=14, column=1, sticky='w')
 
         # Label Simulation Time
-        self.lbl_sim_time = ctk.CTkLabel(self.sim, text='sim_time = 0 s')
-        self.lbl_sim_time.grid(row=12, column=1, sticky='w')
+        self.lbl_sim_time = ctk.CTkLabel(self.sim, text='Simulation Time 0 s')
+        self.lbl_sim_time.grid(row=14, column=2, sticky='w')
 
         # Label Computation Time
-        self.lbl_comp_time = ctk.CTkLabel(self.sim, text='comp_time = 0 s')
-        self.lbl_comp_time.grid(row=13, column=1, sticky='w')
+        self.lbl_comp_time = ctk.CTkLabel(self.sim, text='Last Step Computation Time 0 s')
+        self.lbl_comp_time.grid(row=14, column=3, sticky='w')
 
-        self.sim.title('Swarmalators')
+        ctk.CTkLabel(self.sim, text='D. Kofler, 2022').grid(row=14, column=9)
+
+        # dummy labels
+        ctk.CTkLabel(self.sim, text=' ').grid(row=0, column=0)
+        ctk.CTkLabel(self.sim, text=' ').grid(row=0, column=4)
+        ctk.CTkLabel(self.sim, text=' ').grid(row=0, column=6)
+        ctk.CTkLabel(self.sim, text=' ').grid(row=13, column=0)
 
     def __init_positions_phases(self):
         '''
@@ -282,6 +319,7 @@ class Simulation:
             self.coupling_probability = round(float(self.entry_coupling_probability.get()), 2)
             self.J = round(float(self.entry_J.get()), 2)
             self.K = round(float(self.entry_K.get()), 2)
+            self.alpha = round(float(self.entry_alpha.get()), 2)
             self.simulation_type = str(self.var_plot_type.get())
             return True
         except:
@@ -289,9 +327,9 @@ class Simulation:
             return False
 
     def __update_labels(self):
-        self.lbl_iteration.configure(text=f'iteration = {self.iteration}')
-        self.lbl_sim_time.configure(text=f'sim_time = {round(self.simulaton_time, 1)} s')
-        self.lbl_comp_time.configure(text=f'step_comp_time = {round(self.comp_time, 0)} ms')
+        self.lbl_iteration.configure(text=f'Iteration {self.iteration}')
+        self.lbl_sim_time.configure(text=f'Simulation Time {round(self.simulaton_time, 1)} s')
+        self.lbl_comp_time.configure(text=f'Last Step Computation Time {round(self.comp_time, 0)} ms')
 
     #endregion
 
@@ -373,12 +411,19 @@ class Simulation:
                 for j in range(1, areas):
                     if self.simulation_type == 'positions': t = j * 4.0 / areas - 2.0
                     else: t = round(j * 2.0 * math.pi / areas - math.pi, 2)
-                    self.canvas.create_text(self.plot_size / areas * j + 15.0, self.plot_size / 2.0 + 15.0, text=str(t)) # x axis labels
-                    if j != areas / 2.0: self.canvas.create_text(self.plot_size / 2.0 + 15.0, self.plot_size / areas * j + 15.0, text=str(-t)) # y axis labels
+                    self.canvas.create_text(self.plot_size / areas * j + 15.0, self.plot_size / 2.0 + 15.0, text=str(t)) # x axis tick marks
+                    if j != areas / 2.0: self.canvas.create_text(self.plot_size / 2.0 + 15.0, self.plot_size / areas * j + 15.0, text=str(-t)) # y axis tick marks
                 
             else:
                 self.canvas.create_line(x_x0, x_y0, x_x1, x_y1, dash=(2, 2)) # helper x axis
                 self.canvas.create_line(y_x0, y_y0, y_x1, y_y1, dash=(2, 2)) # helper y axis
+        
+        if self.simulation_type == 'positions':
+            self.canvas.create_text(self.plot_size - 15.0, self.plot_size / 2.0 - 15.0, text='X') # x axis labels
+            self.canvas.create_text(self.plot_size / 2 + 15.0, 15.0, text='Y') # x axis labels
+        else:
+            self.canvas.create_text(self.plot_size - 75.0, self.plot_size / 2.0 - 15.0, text='Polar Angle of Location') # x axis labels
+            self.canvas.create_text(self.plot_size / 2 + 30.0, 15.0, text='Phase') # x axis labels
 
     def __draw_swarmalators(self):
         '''
